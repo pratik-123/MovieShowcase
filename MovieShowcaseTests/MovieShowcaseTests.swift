8a7253ac2822afea71d50520547f2417b1b79c15
Pratik <pratik.lad@outlook.com>
2020-10-10 14:55:19 +0530

    Initial Commit

---
+++ MovieShowcaseTests/MovieShowcaseTests.swift
@@ -0,0 +1,34 @@
+//
+//  MovieShowcaseTests.swift
+//  MovieShowcaseTests
+//
+//  Created by Pratik on 10/10/20.
+//  Copyright © 2020 Pratik Lad. All rights reserved.
+//
+
+import XCTest
+@testable import MovieShowcase
+
+class MovieShowcaseTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
